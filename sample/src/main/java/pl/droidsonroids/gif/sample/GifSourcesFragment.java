0db7bb7ec2ac65abfc7bb7036cbf0d9a9ba2b061
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2015-05-05 21:10:19 +0200

    GifTextureView#init() fixed

--- sample/src/main/java/pl/droidsonroids/gif/sample/GifSourcesFragment.java
+++ sample/src/main/java/pl/droidsonroids/gif/sample/GifSourcesFragment.java
@@ -19,6 +19,9 @@ import java.nio.ByteBuffer;
 
 import pl.droidsonroids.gif.GifDrawable;
 import pl.droidsonroids.gif.GifImageView;
+import pl.droidsonroids.gif.GifTextView;
+import pl.droidsonroids.gif.GifTextureView;
+import pl.droidsonroids.gif.InputSource;
 
 /**
  * Fragment with various GIF sources examples
@@ -94,56 +97,59 @@ public class GifSourcesFragment extends ListFragment {
 
         @Override
         public int getCount() {
-            return mDescriptions.length;
+            return mDescriptions.length*1000;
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
+
             if (convertView == null) {
-                convertView = mInflater.inflate(R.layout.source_item, parent, false);
+                convertView = new GifTextureView(parent.getContext());
+                convertView.setLayoutParams(new ViewGroup.LayoutParams(600, 600));
             }
-            ((TextView) convertView.findViewById(R.id.desc_tv)).setText(mDescriptions[position]);
-            GifImageView gifImageView = (GifImageView) convertView.findViewById(R.id.giv);
-            GifDrawable gd;
-            try {
-                switch (position) {
-                    case 0: //asset
-                        gd = new GifDrawable(getContext().getAssets(), "Animated-Flag-Finland.gif");
-                        break;
-                    case 1: //resource
-                        gd = new GifDrawable(getContext().getResources(), R.drawable.anim_flag_england);
-                        break;
-                    case 2: //byte[]
-                        gd = new GifDrawable(mByteArray);
-                        break;
-                    case 3: //FileDescriptor
-                        gd = new GifDrawable(getContext().getAssets().openFd("Animated-Flag-Greece.gif"));
-                        break;
-                    case 4: //file path
-                        gd = new GifDrawable(mFilePath);
-                        break;
-                    case 5: //File
-                        gd = new GifDrawable(mFile);
-                        break;
-                    case 6: //AssetFileDescriptor
-                        gd = new GifDrawable(getContext().getResources().openRawResourceFd(R.raw.anim_flag_hungary));
-                        break;
-                    case 7: //ByteBuffer
-                        gd = new GifDrawable(mByteBuffer);
-                        break;
-                    case 8: //Uri
-                        gd = new GifDrawable(getContext().getContentResolver(), Uri.parse("file:///" + mFileForUri.getAbsolutePath()));
-                        break;
-                    case 9: //InputStream
-                        gd = new GifDrawable(getContext().getResources().getAssets().open("Animated-Flag-Delaware.gif", AssetManager.ACCESS_RANDOM));
-                        break;
-                    default:
-                        throw new IndexOutOfBoundsException("Invalid source index");
-                }
-                gifImageView.setImageDrawable(gd);
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
+            ((GifTextureView)convertView).setInputSource(new InputSource.ResourcesSource(getResources(), R.drawable.ef));
+//            ((TextView) convertView.findViewById(R.id.desc_tv)).setText(mDescriptions[position]);
+//            GifImageView gifImageView = (GifImageView) convertView.findViewById(R.id.giv);
+//            GifDrawable gd;
+//            try {
+//                switch (position) {
+//                    case 0: //asset
+//                        gd = new GifDrawable(getContext().getAssets(), "Animated-Flag-Finland.gif");
+//                        break;
+//                    case 1: //resource
+//                        gd = new GifDrawable(getContext().getResources(), R.drawable.anim_flag_england);
+//                        break;
+//                    case 2: //byte[]
+//                        gd = new GifDrawable(mByteArray);
+//                        break;
+//                    case 3: //FileDescriptor
+//                        gd = new GifDrawable(getContext().getAssets().openFd("Animated-Flag-Greece.gif"));
+//                        break;
+//                    case 4: //file path
+//                        gd = new GifDrawable(mFilePath);
+//                        break;
+//                    case 5: //File
+//                        gd = new GifDrawable(mFile);
+//                        break;
+//                    case 6: //AssetFileDescriptor
+//                        gd = new GifDrawable(getContext().getResources().openRawResourceFd(R.raw.anim_flag_hungary));
+//                        break;
+//                    case 7: //ByteBuffer
+//                        gd = new GifDrawable(mByteBuffer);
+//                        break;
+//                    case 8: //Uri
+//                        gd = new GifDrawable(getContext().getContentResolver(), Uri.parse("file:///" + mFileForUri.getAbsolutePath()));
+//                        break;
+//                    case 9: //InputStream
+//                        gd = new GifDrawable(getContext().getResources().getAssets().open("Animated-Flag-Delaware.gif", AssetManager.ACCESS_RANDOM));
+//                        break;
+//                    default:
+//                        throw new IndexOutOfBoundsException("Invalid source index");
+//                }
+//                gifImageView.setImageDrawable(gd);
+//            } catch (IOException ex) {
+//                throw new RuntimeException(ex);
+//            }
             return convertView;
         }
     }
--- sample/src/main/java/pl/droidsonroids/gif/sample/MainActivity.java
+++ sample/src/main/java/pl/droidsonroids/gif/sample/MainActivity.java
@@ -1,11 +1,18 @@
 package pl.droidsonroids.gif.sample;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+
+import pl.droidsonroids.gif.GifDrawable;
+import pl.droidsonroids.gif.GifImageView;
 
 /**
  * Main activity, hosts the pager
@@ -21,6 +28,16 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ((ViewPager) findViewById(R.id.main_pager)).setAdapter(new MainPagerAdapter(this));
+    }
+
+    static float x = 0.5f;
+
+    public void test(View view) {
+        GifImageView tv = (GifImageView) view;
+        GifDrawable gifDrawable = (GifDrawable) tv.getDrawable();
+        Bitmap currentFrame = gifDrawable.getCurrentFrame();
+        Bitmap scaledFrame = Bitmap.createScaledBitmap(currentFrame, 800, 600, true);
+        tv.setImageDrawable(new BitmapDrawable(getResources(), currentFrame));
     }
 
     static class MainPagerAdapter extends FragmentStatePagerAdapter {

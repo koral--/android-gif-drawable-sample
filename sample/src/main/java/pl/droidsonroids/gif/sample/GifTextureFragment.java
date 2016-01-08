df22a59d6d8a6b2447b8bee8d5ab062ba52effcf
Karol Wrótniak <koral--@users.noreply.github.com>
2016-01-08 04:28:53 +0100

    fixes for #240

--- sample/src/main/java/pl/droidsonroids/gif/sample/GifTextureFragment.java
+++ sample/src/main/java/pl/droidsonroids/gif/sample/GifTextureFragment.java
@@ -2,6 +2,7 @@ package pl.droidsonroids.gif.sample;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,15 +11,20 @@ import android.view.ViewGroup;
 import pl.droidsonroids.gif.GifTextureView;
 
 public class GifTextureFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.texture, container, false);
+        return inflater.inflate(R.layout.texture, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
             final GifTextureView gifTextureView = (GifTextureView) view.findViewById(R.id.gifTextureView);
             if (!gifTextureView.isHardwareAccelerated()) {
                 view.findViewById(R.id.text_textureview_stub).setVisibility(View.VISIBLE);
             }
         }
-        return view;
     }
 }

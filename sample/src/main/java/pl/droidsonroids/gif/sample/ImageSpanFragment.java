ab0fabd4e5b896ef51a2e5b4111787b5ebc0f403
Karol Wrótniak <koral--@users.noreply.github.com>
2016-03-19 21:46:02 +0100

    gradle wrapper version updated, support library version updated, javadoc corrections for #259

--- sample/src/main/java/pl/droidsonroids/gif/sample/ImageSpanFragment.java
+++ sample/src/main/java/pl/droidsonroids/gif/sample/ImageSpanFragment.java
@@ -1,6 +1,7 @@
 package pl.droidsonroids.gif.sample;
 
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.SpannableStringBuilder;
 import android.text.style.ImageSpan;
@@ -15,37 +16,41 @@ import pl.droidsonroids.gif.GifDrawable;
 
 public class ImageSpanFragment extends BaseFragment implements Drawable.Callback {
 
-    private TextView tv;
+    private TextView mTextView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        tv = new TextView(getActivity());
-        GifDrawable gifDrawable;
+        mTextView = new TextView(getActivity());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            mTextView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+            mTextView.setTextIsSelectable(true);
+        }
+        final GifDrawable gifDrawable;
         try {
             gifDrawable = new GifDrawable(getResources(), R.drawable.anim_flag_england);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        SpannableStringBuilder ssb = new SpannableStringBuilder("test");
+        final SpannableStringBuilder ssb = new SpannableStringBuilder("test");
         gifDrawable.setBounds(0, 0, gifDrawable.getIntrinsicWidth(), gifDrawable.getIntrinsicHeight());
         gifDrawable.setCallback(this);
         ssb.setSpan(new ImageSpan(gifDrawable), 1, 2, 0);
-        tv.setText(ssb);
-        return tv;
+        mTextView.setText(ssb);
+        return mTextView;
     }
 
     @Override
     public void invalidateDrawable(Drawable who) {
-        tv.invalidate();
+        mTextView.invalidate();
     }
 
     @Override
     public void scheduleDrawable(Drawable who, Runnable what, long when) {
-        tv.postDelayed(what, when);
+        mTextView.postDelayed(what, when);
     }
 
     @Override
     public void unscheduleDrawable(Drawable who, Runnable what) {
-        tv.removeCallbacks(what);
+        mTextView.removeCallbacks(what);
     }
 }

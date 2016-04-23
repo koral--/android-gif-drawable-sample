b5dd0420daac70e4fc679f1ff76322c092a33e08
Karol Wrótniak <koral--@users.noreply.github.com>
2016-04-24 00:47:44 +0200

    okhttp replaced with UrlConnection in HttpFragment

--- sample/src/main/java/pl/droidsonroids/gif/sample/HttpFragment.java
+++ sample/src/main/java/pl/droidsonroids/gif/sample/HttpFragment.java
@@ -1,6 +1,5 @@
 package pl.droidsonroids.gif.sample;
 
-import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -9,89 +8,67 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.ResponseBody;
 import pl.droidsonroids.gif.GifTextureView;
 import pl.droidsonroids.gif.InputSource;
 
 public class HttpFragment extends BaseFragment implements View.OnClickListener {
 
-    private static final String URL = "https://raw.githubusercontent.com/koral--/android-gif-drawable-sample/cb2d1f42b3045b2790a886d1574d3e74281de743/sample/src/main/assets/Animated-Flag-Hungary.gif";
-    private final OkHttpClient mOkHttpClient = new OkHttpClient();
-    private GifTextureView mGifTextureView;
-    private Snackbar mSnackbar;
+	private GifTextureView mGifTextureView;
+	private final ExecutorService mExecutorService = Executors.newSingleThreadExecutor();
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-            if (container != null) {
-                Snackbar.make(container, R.string.gif_texture_view_stub_api_level, Snackbar.LENGTH_INDEFINITE).show();
-            }
-            return null;
-        } else {
-            mGifTextureView = (GifTextureView) inflater.inflate(R.layout.http, container, false);
-            loadGif();
-            return mGifTextureView;
-        }
-    }
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+			if (container != null) {
+				Snackbar.make(container, R.string.gif_texture_view_stub_api_level, Snackbar.LENGTH_LONG).show();
+			}
+			return null;
+		} else {
+			mGifTextureView = (GifTextureView) inflater.inflate(R.layout.http, container, false);
+			downloadGif();
+			return mGifTextureView;
+		}
+	}
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        if (mSnackbar != null) {
-            mSnackbar.dismiss();
-        }
-    }
+	@Override
+	public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH && !mGifTextureView.isHardwareAccelerated()) {
+			Snackbar.make(mGifTextureView, R.string.gif_texture_view_stub_acceleration, Snackbar.LENGTH_LONG).show();
+		}
+	}
 
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH && !mGifTextureView.isHardwareAccelerated()) {
-            Snackbar.make(mGifTextureView, R.string.gif_texture_view_stub_acceleration, Snackbar.LENGTH_LONG).show();
-        }
-    }
+	@Override
+	public void onDestroy() {
+		mExecutorService.shutdownNow();
+		super.onDestroy();
+	}
 
-    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-    private void loadGif() {
-        final Call call = mOkHttpClient.newCall(new Request.Builder().url(URL).build());
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(final Call call, final IOException e) {
-                if (isDetached()) {
-                    return;
-                }
-                mGifTextureView.setOnClickListener(HttpFragment.this);
-                final String message = getString(R.string.gif_texture_view_loading_failed, e.getMessage());
-                mSnackbar = Snackbar.make(mGifTextureView, message, Snackbar.LENGTH_LONG);
-                mSnackbar.show();
-            }
+	void onGifDownloaded(ByteBuffer buffer) {
+		mGifTextureView.setInputSource(new InputSource.DirectByteBufferSource(buffer));
+	}
 
-            @Override
-            public void onResponse(final Call call, final Response response) throws IOException {
-                if (isDetached()) {
-                    return;
-                }
-                if (!response.isSuccessful()) {
-                    onFailure(call, new IOException(response.message()));
-                    return;
-                }
-                final ResponseBody body = response.body();
-                mGifTextureView.setInputSource(new InputSource.ByteArraySource(body.bytes()));
-                mGifTextureView.setOnClickListener(null);
-            }
-        });
-    }
+	void onDownloadFailed(Exception e) {
+		mGifTextureView.setOnClickListener(HttpFragment.this);
+		if (isDetached()) {
+			return;
+		}
+		final String message = getString(R.string.gif_texture_view_loading_failed, e.getMessage());
+		Snackbar.make(mGifTextureView, message, Snackbar.LENGTH_LONG).show();
 
-    @Override
-    public void onClick(View v) {
-        loadGif();
-        mSnackbar.dismiss();
-    }
+	}
+
+	private void downloadGif() {
+		mExecutorService.submit(new GifLoadTask(this));
+	}
+
+	@Override
+	public void onClick(View v) {
+		downloadGif();
+	}
 }
